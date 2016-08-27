a0af0cf7489126c72b2c0aa2d415c626d958c701
omriysh <omri.ysh@gmail.com>
2016-08-27 08:19:42 +0300

    Threading!
    Added task system with basic implementation

--- Robots vs Mummies - Space Pyramids/Assets/Scripts/Platform.cs
+++ Robots vs Mummies - Space Pyramids/Assets/Scripts/Platform.cs
@@ -3,6 +3,10 @@ using System.Collections;
 
 public class Platform : MonoBehaviour
 {
+    public int level = 0;
+    public Transform ladderUp = null;
+    public Transform ladderDown = null;
+
     void OnTriggerEnter2D(Collider2D other)
     {
         Mummy m = other.transform.GetComponent<Mummy>();
