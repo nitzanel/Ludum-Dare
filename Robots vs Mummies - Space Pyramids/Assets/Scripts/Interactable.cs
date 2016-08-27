8f258f0ae9dc00cddd2d255fc2ba0ba4b0998b99
omriysh <omri.ysh@gmail.com>
2016-08-27 18:19:32 +0300

    Now Introducing... Interactables!
    Fully working system of items and objects, comes with a functionul
    furnice and a wood pile.

---
+++ Robots vs Mummies - Space Pyramids/Assets/Scripts/Interactable.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Interactable : MonoBehaviour
+{
+    public void OnMouseDown()
+    {
+        if (Input.GetKey("mouse 0"))
+        {
+            GameObject[] mummies = GameObject.FindGameObjectsWithTag("Mummy");
+            foreach (GameObject mummy in mummies)
+            {
+                Mummy m = mummy.GetComponent<Mummy>();
+                if (m.isSelected)
+                {
+                    Action(m);
+                    if (m.marker)
+                        StartCoroutine("WaitAndMove", m.marker);
+                }
+            }
+        }
+    }
+
+    IEnumerator WaitAndMove(Transform marker)
+    {
+        yield return new WaitForEndOfFrame();
+        marker.position = new Vector3(transform.position.x, transform.position.y + transform.GetComponent<SpriteRenderer>().bounds.size.y / 2, 0);
+    }
+
+    public virtual void Action(Mummy m)
+    {
+
+    }
+
+    public virtual void Interact()
+    {
+        
+    }
+}
+
+public class Item
+{
+    public string name;
+    public int amount;
+
+    public Item(string Name, int Amount = 1)
+    {
+        name = Name;
+        amount = Amount;
+    }
+}
