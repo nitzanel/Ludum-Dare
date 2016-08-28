0e7c312b2683c1dd27b120c531c497118788917d
omriysh <omri.ysh@gmail.com>
2016-08-28 16:54:00 +0300

    Started Developing Enemies
    Enemy base class created, enemy manager created and it spawns the ship
    that does nothing. It also moves the pyramid slowly but surely.

---
+++ Robots vs Mummies - Space Pyramids/Assets/Scripts/Enemy.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Enemy : MonoBehaviour
+{
+    public List<Transform> soldiers; //list of prefabs the enemy will spawn (can be empty)
+    public float spawnInterval = 5;
+    public float attackInterval = 2;
+    public float speed = 0.1f;
+    public int hp = 10;
+
+    protected float time = 0;
+
+	void Start ()
+    {
+	
+	}
+	
+	void Update ()
+    {
+        if ((time += Time.deltaTime) >= spawnInterval) Spawn();
+	}
+
+    protected virtual void Spawn()
+    {
+
+    }
+
+    protected virtual void Attack()
+    {
+
+    }
+
+    public virtual void Hit()
+    {
+
+    }
+}
